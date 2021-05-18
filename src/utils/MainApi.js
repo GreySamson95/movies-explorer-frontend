ade62b5e53f8bea92dcf48741727d121a3f81c4d
GreySamson95 <greyfullbaster.gs@gmail.com>
2021-05-18 09:59:40 +0300

    fix

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -4,77 +4,72 @@ class MainApi {
     this._baseUrl = baseUrl;
   }
 
-  // _fetchAndCatch(url, payload) {
-  //   const checkStatus = async (response) => {
-  //     if (response.status >= 200 && response.status < 300)
-  //       return await response.json()
+  _fetchAndCatch(url, payload) {
+    const checkStatus = async (response) => {
+      if (response.status >= 200 && response.status < 300)
+        return await response.json()
 
-  //     throw await response.json()
-  //   }
-  //   return fetch(url, payload)
-  //     .then(checkStatus)
-  // }
-  async errorCheck(res) {
-    const msg = await res.json();
-    return res.ok ? msg : Promise.reject(msg);
+      throw await response.json()
+    }
+    return fetch(url, payload)
+      .then(checkStatus)
   }
-
 
 
   signUpUser(formData) {
-    return fetch(`${this._baseUrl}/signup`, {
+    return this._fetchAndCatch(`${this._baseUrl}/signup`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(formData),
-    }).then(this.errorCheck);
+    });
   }
 
   signInUser(formData) {
-    return fetch(`${this._baseUrl}/signin`, {
+    return this._fetchAndCatch(`${this._baseUrl}/signin`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(formData),
-    }).then(this.errorCheck);
+    });
   }
 
   getUser(jwt) {
     this._jwt = jwt;
-    return fetch(`${this._baseUrl}/users/me`, {
+    return this._fetchAndCatch(`${this._baseUrl}/users/me`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${jwt}`,
       },
-    }).then(this.errorCheck);
+    });
   }
 
   updateUser(newData, jwt) {
-    return fetch(`${this._baseUrl}/users/me`, {
+    return this._fetchAndCatch(`${this._baseUrl}/users/me`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${jwt}`,
       },
       body: JSON.stringify(newData),
-    }).then(this.errorCheck);
+    });
   }
 
   getFavouriteMovies(jwt) {
-    return fetch(`${this._baseUrl}/movies`, {
+    return this._fetchAndCatch(`${this._baseUrl}/movies`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${jwt}`,
       },
-    }).then(this.errorCheck);
+    });
   }
 
   likeMovie(movie, jwt) {
-    return fetch(`${this._baseUrl}/movies`, {
+    return this._fetchAndCatch(`${this._baseUrl}/movies`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -93,23 +88,23 @@ class MainApi {
         nameEN: movie.nameEN,
         thumbnail: `https://api.nomoreparties.co/uploads${movie.image.url}`,
       }),
-    }).then(this.errorCheck);
+    });
   }
 
   deleteMovieLike(id, jwt) {
-    return fetch(`${this._baseUrl}/movies/${id}`, {
+    return this._fetchAndCatch(`${this._baseUrl}/movies/${id}`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${jwt}`,
       },
-    }).then(this.errorCheck);
+    });
   }
 }
 
 const mainApi = new MainApi({
-  baseUrl: 'https://api.greysamson.nomoredomains.club',
-  // baseUrl: 'http://localhost:3000',
+  // baseUrl: 'https://api.greysamson.nomoredomains.club',
+  baseUrl: 'http://localhost:3000',
 });
 
 export default mainApi;
